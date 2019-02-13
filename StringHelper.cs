64eaac61eeff4cf1472207a5c23e55bac4378b62
vladislav-silitskiy <vladislav.silitskiy@gmail.com>
2019-02-13 21:58:13 +0300

    Update StringHelper.cs

--- StringHelper.cs
+++ StringHelper.cs
@@ -7,7 +7,39 @@ namespace PadawansTask4
         public static int GetVowelCount(string str)
         {
             // put your code here
-            throw new NotImplementedException();
+            int count = 0;
+
+            foreach (char ch in str)
+            {
+                if (ch == 'a')
+                    count++;
+            }
+
+            foreach (char ch in str)
+            {
+                if (ch == 'e')
+                    count++;
+            }
+
+            foreach (char ch in str)
+            {
+                if (ch == 'i')
+                    count++;
+            }
+
+            foreach (char ch in str)
+            {
+                if (ch == 'o')
+                    count++;
+            }
+
+            foreach (char ch in str)
+            {
+                if (ch == 'u')
+                    count++;
+            }
+
+            return count;
         }
     }
 }
